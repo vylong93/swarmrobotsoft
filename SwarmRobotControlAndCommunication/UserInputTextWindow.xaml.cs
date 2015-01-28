a2b64949317384a7c2d0ded620eaa0d191300a04
vylong93khtn@gmail.com <vylong93khtn@gmail.com@e8cdf95c-477e-7508-a944-48273b80800c>
2015-01-28 03:50:17 +0000

    addition UserInputTextWindow form

---
+++ SwarmRobotControlAndCommunication/UserInputTextWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SwarmRobotControlAndCommunication
+{
+    /// <summary>
+    /// Interaction logic for UserInputTextWindow.xaml
+    /// </summary>
+    public partial class UserInputTextWindow : Window
+    {
+        private bool userConfirm;
+
+        public UserInputTextWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void setMessage(string msg)
+        {
+            messageLable.Content = msg;
+        }
+
+        public string inputText
+        {
+            get { return inputTextBox.Text; }
+        }
+
+        public bool UserConfirm
+        {
+            get { return userConfirm; }
+        }
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            userConfirm = true;
+            this.Close();
+        }
+
+        private void cancleButton_Click(object sender, RoutedEventArgs e)
+        {
+            userConfirm = false;
+            this.Close();
+        }
+    }
+}
