d603d77e8594e22e04e65d14248143c78478e666
vylong93khtn@gmail.com <vylong93khtn@gmail.com@e8cdf95c-477e-7508-a944-48273b80800c>
2015-01-30 05:17:17 +0000

    feature fix for read ADC1, ADC2 modified robot's motor testing feature

--- trunk/SwarmRobotControlAndCommunication/TM4C123ControlBoard.cs
+++ trunk/SwarmRobotControlAndCommunication/TM4C123ControlBoard.cs
@@ -411,19 +411,19 @@ namespace SwarmRobotControlAndCommunication
             outputBuffer[1] = RECEIVE_DATA_FROM_ROBOT_CONTINUE;
 
             //TODO: fix DUMMY read ------------------
-            inputBuffer = readDataFromControlBoard();
+            //inputBuffer = readDataFromControlBoard();
 
-            if (numberOfReceivedBytes <= MAX_NUM_BYTE_RECEIVED)
-            {
-                checkIfReceivedDataFromRobot(inputBuffer);
+            //if (numberOfReceivedBytes <= MAX_NUM_BYTE_RECEIVED)
+            //{
+            //    checkIfReceivedDataFromRobot(inputBuffer);
 
-                for (UInt32 j = 1; j <= numberOfReceivedBytes; j++)
-                {
-                    data[pointer] = inputBuffer[j];
-                    pointer++;
-                }
-                return;
-            }
+            //    for (UInt32 j = 1; j <= numberOfReceivedBytes; j++)
+            //    {
+            //        data[pointer] = inputBuffer[j];
+            //        pointer++;
+            //    }
+            //    return;
+            //}
             //TODO: fix ------------------ DUMMY read
 
             while (true)
