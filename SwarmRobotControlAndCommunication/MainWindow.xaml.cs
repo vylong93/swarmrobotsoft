f96b5cc5a53dce0d5c3c62ddecbe307075b8a4c8
vylong93khtn@gmail.com <vylong93khtn@gmail.com@e8cdf95c-477e-7508-a944-48273b80800c>
2015-02-13 14:31:03 +0000

    EEPROM tab's feature update completed: Program & Check Sine/ArcSine table

--- SwarmRobotControlAndCommunication/MainWindow.xaml.cs
+++ SwarmRobotControlAndCommunication/MainWindow.xaml.cs
@@ -69,8 +69,8 @@ namespace SwarmRobotControlAndCommunication
 
         private const byte COMMAND_EEPROM_DATA_READ = 0x10;
         private const byte COMMAND_EEPROM_DATA_WRITE = 0x11;
-        private const byte COMMAND_EEPROM_TABLE_READ = 0x12;
-        private const byte COMMAND_EEPROM_TABLE_UPDATE = 0x13;
+        private const byte COMMAND_EEPROM_DATA_READ_BULK = 0x12;
+        private const byte COMMAND_EEPROM_DATA_WRITE_BULK = 0x13;
         //==== out
 
         private const byte COMMAND_SET_RUNNING_STATUS = 0xC3;
@@ -105,6 +105,103 @@ namespace SwarmRobotControlAndCommunication
         //---------------------------------Commands to control all Robots
         #endregion
 
+        #region EEPROM Table
+     
+        #region Sine Table Initilization - [0 ---> 1] step = 0.5 degree, offset *32768
+     
+        private UInt16[] SineTableBlock0 = {	// EEPROM start address Block 2 = 0x0080
+	        0,    286,  572,  858,  1144, 1429, 1715, 2000,
+	        2286, 2571, 2856, 3141, 3425, 3709, 3993, 4277,
+	        4560, 4843, 5126, 5408, 5690, 5971, 6252, 6533,
+	        6813, 7092, 7371, 7650, 7927, 8204, 8481, 8757
+        };
+
+        private UInt16[] SineTableBlock1 = {	// EEPROM start address Block 3 = 0x00c0
+	        9032,  9307,  9580,  9854,  10126, 10397, 10668, 10938,
+	        11207, 11476, 11743, 12010, 12275, 12540, 12803, 13066,
+	        13328, 13589, 13848, 14107, 14365, 14621, 14876, 15131,
+	        15384, 15636, 15886, 16136, 16384, 16631, 16877, 17121
+        };
+
+        private UInt16[] SineTableBlock2 = {	// EEPROM start address Block 4 = 0x0100
+	        17364, 17606, 17847, 18086, 18324, 18560, 18795, 19028,
+	        19261, 19491, 19720, 19948, 20174, 20399, 20622, 20843,
+	        21063, 21281, 21498, 21713, 21926, 22138, 22348, 22556,
+	        22763, 22967, 23170, 23372, 23571, 23769, 23965, 24159
+        };
+
+        private UInt16[] SineTableBlock3 = {	// EEPROM start address Block 5 = 0x0140
+	        24351, 24542, 24730, 24917, 25102, 25285, 25466, 25645,
+	        25822, 25997, 26170, 26341, 26510, 26677, 26842, 27005,
+	        27166, 27325, 27482, 27636, 27789, 27939, 28088, 28234,
+	        28378, 28520, 28660, 28797, 28932, 29066, 29197, 29325
+        };
+
+        private UInt16[] SineTableBlock4 = {	// EEPROM start address Block 6 = 0x0180
+	        29452, 29576, 29698, 29818, 29935, 30050, 30163, 30274,
+	        30382, 30488, 30592, 30693, 30792, 30888, 30983, 31075,
+	        31164, 31251, 31336, 31419, 31499, 31576, 31651, 31724,
+	        31795, 31863, 31928, 31991, 32052, 32110, 32166, 32219
+        };
+
+        private UInt16[] SineTableBlock5 = {	// EEPROM start address Block 7 = 0x01c0
+	        32270, 32319, 32365, 32408, 32449, 32488, 32524, 32557,
+	        32588, 32617, 32643, 32667, 32688, 32707, 32723, 32737,
+	        32748, 32757, 32763, 32767, 32768, 0,     0,     0,
+	        0, 	   0, 	  0,     0,     0,     0,     0,     0
+        };
+
+        #endregion
+
+        #region ArcSine Table Initilization - [0 ---> pi / 2] step = 1/180, offset *32768
+
+        private UInt16[] ArcSineTableBlock0 = {	// EEPROM start address Block 8 = 0x0200
+	        0,    182,  364,  546,  728,  910,  1092, 1275,
+	        1457, 1639, 1821, 2004, 2186, 2369, 2551, 2734,
+	        2917, 3099, 3282, 3465, 3648, 3832, 4015, 4199,
+	        4382, 4566, 4750, 4934, 5118, 5302, 5487, 5672
+        };
+
+        private UInt16[] ArcSineTableBlock1 = {	// EEPROM start address Block 9 = 0x0240
+	        5857,  6042,  6227,  6412,  6598,  6784,  6970,  7156,
+	        7343,  7530,  7717,  7904,  8092,  8280,  8468,  8656,
+	        8845,  9034,  9224,  9413,  9603,  9794,  9984,  10175,
+	        10367, 10558, 10750, 10943, 11136, 11329, 11523, 11717
+        };
+
+        private UInt16[] ArcSineTableBlock2 = {	// EEPROM start address Block 10 = 0x0280
+	        11911, 12106, 12302, 12498, 12694, 12891, 13088, 13286,
+	        13485, 13683, 13883, 14083, 14283, 14485, 14686, 14889,
+	        15091, 15295, 15499, 15704, 15909, 16116, 16323, 16530,
+	        16738, 16947, 17157, 17368, 17579, 17791, 18005, 18218
+        };
+
+        private UInt16[] ArcSineTableBlock3 = {	// EEPROM start address Block 11 = 0x02c0
+	        18433, 18649, 18865, 19083, 19301, 19521, 19741, 19963,
+	        20185, 20409, 20633, 20859, 21086, 21314, 21544, 21774,
+	        22006, 22239, 22474, 22710, 22947, 23186, 23426, 23668,
+	        23912, 24157, 24404, 24652, 24902, 25154, 25408, 25664
+        };
+
+        private UInt16[] ArcSineTableBlock4 = {	// EEPROM start address Block 12 = 0x0300
+	        25922, 26182, 26444, 26708, 26975, 27244, 27515, 27789,
+	        28066, 28345, 28627, 28912, 29200, 29492, 29786, 30084,
+	        30386, 30691, 31000, 31313, 31631, 31953, 32280, 32612,
+	        32949, 33292, 33640, 33995, 34357, 34725, 35101, 35485
+        };
+
+        private UInt16[] ArcSineTableBlock5 = {	// EEPROM start address Block 13 = 0x0340
+	        35878, 36280, 36693, 37116, 37551, 38000, 38463, 38942,
+	        39439, 39957, 40498, 41066, 41666, 42303, 42988, 43730,
+	        44551, 45481, 46583, 48016, 51472, 0,     0,     0,
+	        0, 	   0, 	  0,     0,     0,     0,     0,     0
+        };
+        #endregion
+
+        private List<Tuple<UInt32, UInt16[]>> listTupeEepromTable;
+
+        #endregion
+
         #region Constructors
 
         /// <summary>
@@ -132,6 +229,22 @@ namespace SwarmRobotControlAndCommunication
             this.TXAdrrTextBox.Text = DEFAULT_TX_ADDRESS;
             this.Pipe0AddressTextBox.Text = DEFAULT_RX_ADDRESS;
             this.TXAdrrTextBoxDebug.Text = DEFAULT_TX_ADDRESS;
+
+            listTupeEepromTable = new List<Tuple<UInt32, UInt16[]>>();
+
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x0080, SineTableBlock0));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x00c0, SineTableBlock1));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x0100, SineTableBlock2));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x0140, SineTableBlock3));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x0180, SineTableBlock4));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x01c0, SineTableBlock5));
+
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x0200, ArcSineTableBlock0));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x0240, ArcSineTableBlock1));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x0280, ArcSineTableBlock2));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x02C0, ArcSineTableBlock3));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x0300, ArcSineTableBlock4));
+            listTupeEepromTable.Add(new Tuple<uint, ushort[]>(0x0340, ArcSineTableBlock5));
         }
 
         private void usbEvent_receiver(object o, EventArgs e)
@@ -794,21 +907,7 @@ namespace SwarmRobotControlAndCommunication
 
         private void EepromTableVerifyButton_Click(object sender, RoutedEventArgs e)
         {
-            //byte[] testPacket = new byte[120];
-            //for (int i = 0; i < testPacket.Length; i++)
-            //    testPacket[i] = (byte)(i + 1);
-
-            //if (theControlBoard.sendPacketToControlBoard(testPacket))
-            //{
-            //    setStatusBarContent("OK!");
-            //}
-            //else
-            //{
-            //    setStatusBarContent("Failed...");
-            //}
-
-            //TODO: uncomment this line below
-            //assignTaskForBackgroundWorker((Button)sender, "Cancel Verify");
+            assignTaskForBackgroundWorker((Button)sender, "Cancel Verify");
         }
 
         private void EepromRandomGenButton_Click(object sender, RoutedEventArgs e)
@@ -966,37 +1065,105 @@ namespace SwarmRobotControlAndCommunication
         }
         private void tableVerifying_DoWork(object sender, DoWorkEventArgs e)
         {
-            //TODO: put your task here
+            const byte TOTAL_BLOCK = 12; // 1->6 in Sine Table; 7->12 is ArcSine Table
+            const byte NUMBER_OF_WORD = 16;
+            byte currentBlock = 1;
+            byte[] data = new byte[1 + 4];
+            data[0] = NUMBER_OF_WORD;
 
-            //Test only-------------------------------------------------------
-            int count = 10; 
-            int sum = 0;
-            for (int i = 1; i <= count; i++)
+            UInt32 bufferLength = 2 + 1 + 4 + 4 * NUMBER_OF_WORD;
+            byte[] dataBuffer = new byte[bufferLength];
+            UInt32 rxAddress;
+
+            try
             {
-                if (backgroundWorker.CancellationPending)
+                for (currentBlock = 1; currentBlock <= TOTAL_BLOCK; currentBlock++)
                 {
-                    e.Cancel = true;
-                    break;
+                    if (backgroundWorker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+
+                    UInt32 address = listTupeEepromTable[currentBlock - 1].Item1;
+
+                    // fill Address
+                    data[1] = (byte)(address >> 24);
+                    data[2] = (byte)(address >> 16);
+                    data[3] = (byte)(address >> 8);
+                    data[4] = (byte)address;
+
+                    SwarmMessageHeader header = new SwarmMessageHeader(e_MessageType.MESSAGE_TYPE_HOST_COMMAND, COMMAND_EEPROM_DATA_READ_BULK);
+                    SwarmMessage message = new SwarmMessage(header, data);
+
+                    theControlBoard.receivedDataFromRobot(dataBuffer, bufferLength, 1000, message);
+
+                    SwarmMessage rxMessage = SwarmMessage.ConstructFromByteArray(dataBuffer);
+
+                    byte[] messageContent;
+
+                    if (rxMessage.getHeader().getMessageType() == e_MessageType.MESSAGE_TYPE_ROBOT_RESPONSE
+                        && rxMessage.getHeader().getCmd() == ROBOT_RESPONSE_OK)
+                    {
+                        messageContent = rxMessage.getData();
+                        UInt16[] pui16TableContent = listTupeEepromTable[currentBlock - 1].Item2;
+
+                        rxAddress = (UInt32)((messageContent[1] << 24) | (messageContent[2] << 16) | 
+                                    (messageContent[3] << 8) | messageContent[4]);
+
+                        if (messageContent[0] == NUMBER_OF_WORD && rxAddress == listTupeEepromTable[currentBlock - 1].Item1)
+                        { 
+                            UInt16[] referenceContent = listTupeEepromTable[currentBlock - 1].Item2;
+                            UInt32 contentPointer = 5;
+                            for (int i = 0; i < referenceContent.Length; i++)
+                            {
+                                if ((messageContent[contentPointer] != (referenceContent[i] & 0xFF)) ||
+                                    (messageContent[contentPointer + 1] != ((referenceContent[i] >> 8)& 0xFF)))
+                                {
+                                    if (currentBlock > (TOTAL_BLOCK / 2))
+                                        e.Result = e_VerifyTableReturn.SINE_OK_ARCSINE_ERROR;
+                                    else
+                                        e.Result = e_VerifyTableReturn.SINE_ERROR_ARCSINE_ERROR;
+                                    return;
+                                }
+
+                                contentPointer += 2;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+  
+                        backgroundWorker.ReportProgress(currentBlock * 100 / TOTAL_BLOCK);
+
+                        System.Threading.Thread.Sleep(500);
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
-
-                sum += i;
-
-                backgroundWorker.ReportProgress(i * 100 / count);
-
-                System.Threading.Thread.Sleep(300);
+            }
+            catch (Exception ex)
+            {
+                defaultExceptionHandle(ex);
             }
 
-            e.Result = e_VerifyTableReturn.SINE_ERROR_ARCSINE_ERROR;
-            //-------------------------------------------------------Test only
+            if (currentBlock >= TOTAL_BLOCK)
+                e.Result = e_VerifyTableReturn.SINE_OK_ARCSINE_OK;
+            else
+                e.Result = e_VerifyTableReturn.SINE_ERROR_ARCSINE_ERROR;
         }
         private void tableProgramming_DoWork(object sender, DoWorkEventArgs e)
         {
-            //TODO: put your task here
+            const byte TOTAL_BLOCK = 12; // 1->6 in Sine Table; 7->12 is ArcSine Table
+            const byte NUMBER_OF_WORD = 16;
+            byte currentBlock = 1;
+            byte[] data = new byte[1 + 4 + 4 * NUMBER_OF_WORD];
+            data[0] = NUMBER_OF_WORD;
 
-            //Test only-------------------------------------------------------
-            int count = 28;
-            int sum = 0;
-            for (int i = 1; i <= count; i++)
+            for (currentBlock = 1; currentBlock <= TOTAL_BLOCK; currentBlock++)
             {
                 if (backgroundWorker.CancellationPending)
                 {
@@ -1004,15 +1171,33 @@ namespace SwarmRobotControlAndCommunication
                     break;
                 }
 
-                sum += i;
+                UInt32 address = listTupeEepromTable[currentBlock - 1].Item1;
+                UInt16[] pui16Data = listTupeEepromTable[currentBlock - 1].Item2;
 
-                backgroundWorker.ReportProgress(i * 100 / count);
+                fillStartAddressAndWordContent(address, pui16Data, data, 1);
 
-                System.Threading.Thread.Sleep(300);
+                SwarmMessageHeader header = new SwarmMessageHeader(e_MessageType.MESSAGE_TYPE_HOST_COMMAND, COMMAND_EEPROM_DATA_WRITE_BULK);
+                SwarmMessage message = new SwarmMessage(header, data);
+
+                if (!theControlBoard.sendMessageToRobot(message))
+                {
+                    if (currentBlock > (TOTAL_BLOCK / 2))
+                        e.Result = e_VerifyTableReturn.SINE_OK_ARCSINE_ERROR;
+                    else
+                        e.Result = e_VerifyTableReturn.SINE_ERROR_ARCSINE_ERROR;
+
+                    return;
+                }
+
+                backgroundWorker.ReportProgress(currentBlock  * 100 / TOTAL_BLOCK);
+
+                System.Threading.Thread.Sleep(500);
             }
 
-            e.Result = e_VerifyTableReturn.SINE_ERROR_ARCSINE_ERROR;
-            //-------------------------------------------------------Test only
+            if(currentBlock >= TOTAL_BLOCK)
+                e.Result = e_VerifyTableReturn.SINE_OK_ARCSINE_OK;
+            else
+                e.Result = e_VerifyTableReturn.SINE_ERROR_ARCSINE_ERROR;
         }
 
         private void backgroundWorkerTableManipulation_ProgressChanged(object sender, ProgressChangedEventArgs e)
@@ -1100,6 +1285,22 @@ namespace SwarmRobotControlAndCommunication
             backgroundWorker.ProgressChanged -= backgroundWorkerTableManipulation_ProgressChanged;
         }
         #endregion
+
+        private void fillStartAddressAndWordContent(UInt32 address, UInt16[] pui16ContentBuffer, byte[] data, uint offset)
+        {
+            // fill Address
+            data[offset++] = (byte)(address >> 24);
+            data[offset++] = (byte)(address >> 16);
+            data[offset++] = (byte)(address >> 8);
+            data[offset++] = (byte)address;
+           
+            // fill Word content
+            for (int i = 0; i < pui16ContentBuffer.Length; i++)
+            {
+                data[offset++] = (byte)(pui16ContentBuffer[i] & 0xFF);
+                data[offset++] = (byte)((pui16ContentBuffer[i] >> 8) & 0xFF);
+            }
+        }
 
         #endregion
 
